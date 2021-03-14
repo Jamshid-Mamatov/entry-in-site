80adc489bde6e5e939ddb738cc836012ffd11afa
Jamshid <mamatovjamshid506@gmail.com>
2021-03-14 10:55:18 +0500

    sign up

--- register.py
+++ register.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.exceptions import default_exceptions
 
@@ -19,6 +19,29 @@ class User(db.Model):
 def home():
     content="home site"
     return render_template("home.html",content=content)
+
+
+@app.route("/signup")
+def sign_up():
+    content="Sign Up"
+    position="get_signup"
+    return render_template("entry.html",content=content,position=position)
+
+@app.route("/get_signup",methods=["POST"])
+def get_sign_up():
+    content="register"
+    r=request.form
+    get_email=r.get("email")
+    get_username=r.get("username")
+    get_password=r.get("password")
+    get_first_name=r.get("first_name")
+    get_last_name=r.get("last_name")
+
+    user=User(email=get_email,username=get_username,password=get_password,first_name=get_first_name,last_name=get_last_name)
+
+    db.session.add(user)
+    db.session.commit()
+    return "ok"
 
 
 if __name__=="__main__":
